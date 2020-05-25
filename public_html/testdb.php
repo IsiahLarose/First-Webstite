1c3ce0374a5f5e16e0bdc1499d01348d58e64ad5
IsiahLarose <65555414+IsiahLarose@users.noreply.github.com>
2020-05-25 17:58:07 -0400

    Update testdb.php

--- public_html/testdb.php
+++ public_html/testdb.php
@@ -1,19 +1,21 @@
 <?php
 require("config.php");
-$connection_String = "mysql:host=$dbhost;dbname=dbdatabase;charset=utf8mb4";
-try {
-    $db = new PDO($connection_String, $dbhost, $dbuser, $dbpass);
-    $stmt = $db->prepare("CREATE TABLE `Users` (
-                 `id` int auto_increment not null,
-                 `email` varchar(100) not null unique,
-                 `created` timestamp default current_timestamp,
-                PRIMARY KEY (`id`)
-                 ) CHARACTER SET utf8 COLLATE utf8_general_ci");
-    $r = $stmt->execute();
-    echo var_export($stmt->errorInfo(), true);
-    echo var_export($r, true);
+
+$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+try{
+	$db = new PDO($connection_string, $dbuser, $dbpass);
+	$stmt = $db->prepare("CREATE TABLE `Users` (
+				`id` int auto_increment not null,
+				`email` varchar(100) not null unique,
+				`created` timestamp default current_timestamp,
+				`modified` timestamp default current_timestamp on update current_timestamp,
+				PRIMARY KEY (`id`)
+				) CHARACTER SET utf8 COLLATE utf8_general_ci");
+	$r = $stmt->execute();
+	echo var_export($stmt->errorInfo(), true);
+	echo var_export($r, true);
 }
-catch   (Exception $e){
-    echo $e->getMessage();
+catch (Exception $e){
+	echo $e->getMessage();
 }
 ?>
