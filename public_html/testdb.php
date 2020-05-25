998b8b1592289ec2cd77f1c1b7faf6d47253f582
IsiahLarose <iel3@njit.edu>
2020-05-25 16:13:16 -0400

    wrong quote marks

--- public_html/testdb.php
+++ public_html/testdb.php
@@ -3,11 +3,11 @@ require("config.php");
 $connection_String = "mysql:host=$dbhost;dbname+$dbdatabase;charset=utf8mb4";
 try {
     $db = new PDO($connection_string, $dbuser, $dbpass);
-    $stmt = $db->prepare("CREATE TABLE 'Users' (
-                 'id' int auto_increment not null,
-                 'email' varchar(100) not null unique,
-                 'created' timestamp default current_timestamp,
-                 PRIMARY KEY ('id')
+    $stmt = $db->prepare("CREATE TABLE `Users` (
+                 `id` int auto_increment not null,
+                 `email` varchar(100) not null unique,
+                 `created` timestamp default current_timestamp,
+                 PRIMARY KEY (`id`)
                  )CHARACTER SET utf8 COLLATE utf8_general_ci");
     $r = $stmt->execute();
     echo var_export($stmt->errorInfo(), true);
