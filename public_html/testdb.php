d21c070d32e262230313dc1106c4e80f1673c121
IsiahLarose <iel3@njit.edu>
2020-05-25 15:43:50 -0400

    test connection

--- public_html/testdb.php
+++ public_html/testdb.php
@@ -1,4 +1,10 @@
 <?php
 require("config.php");
-echo $dbhost;
+$connection_String = "mysql:host=$dbhost;dbname+$dbdatabase;charset=utf8mb4";
+try {
+    $db = new PDO($connection_string, $dbuser, $dbpass);
+}
+catch   (Exception $e){
+    echo $e->getMessage();
+}
 ?>
