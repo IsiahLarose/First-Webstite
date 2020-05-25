fa27864a545fd7b8ba1d1efb578a286fb8e67807
IsiahLarose <iel3@njit.edu>
2020-05-25 16:19:40 -0400

    changed plus to equal

--- public_html/testdb.php
+++ public_html/testdb.php
@@ -1,6 +1,6 @@
 <?php
 require("config.php");
-$connection_String = "mysql:host=$dbhost;dbname+$dbdatabase;charset=utf8mb4";
+$connection_String = "mysql:host=$dbhost;dbname=dbdatabase;charset=utf8mb4";
 try {
     $db = new PDO($connection_string, $dbuser, $dbpass);
     $stmt = $db->prepare("CREATE TABLE `Users` (
@@ -8,7 +8,7 @@ try {
                  `email` varchar(100) not null unique,
                  `created` timestamp default current_timestamp,
                  PRIMARY KEY (`id`)
-                 )CHARACTER SET utf8 COLLATE utf8_general_ci");
+                 ) CHARACTER SET utf8 COLLATE utf8_general_ci");
     $r = $stmt->execute();
     echo var_export($stmt->errorInfo(), true);
     echo var_export($r, true);
