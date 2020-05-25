e335b72e1cc13f7a2fef54dc6f8d1405a4698988
IsiahLarose <iel3@njit.edu>
2020-05-25 17:46:45 -0400

    added host

--- public_html/config.php
+++ public_html/config.php
@@ -1,7 +1,7 @@
 <?php
 $cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$dbhost   = $cleardb_url["host"];
-$db_user = $cleardb_url["user"];
-$dbpass = $cleardb_url["pass"];
-$dbdatabase       = substr($cleardb_url["path"],1);
+global $dbhost   = $cleardb_url["host"];
+global $db_user = $cleardb_url["user"];
+global $dbpass = $cleardb_url["pass"];
+global $dbdatabase       = substr($cleardb_url["path"],1);
 ?>
