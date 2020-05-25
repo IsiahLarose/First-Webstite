c46e37b485de7e9e71484743113c9e4d290c0ef2
IsiahLarose <iel3@njit.edu>
2020-05-25 16:26:25 -0400

    undid changes

--- public_html/testdb.php
+++ public_html/testdb.php
@@ -1,5 +1,5 @@
 <?php
-require("config");
+require("config.php");
 $connection_String = "mysql:host=$dbhost;dbname=dbdatabase;charset=utf8mb4";
 try {
     $db = new PDO($connection_string, $dbuser, $dbpass);
