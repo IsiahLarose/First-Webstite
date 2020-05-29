d75774f36118366ade918804dbd029666055cc11
IsiahLarose <iel3@njit.edu>
2020-05-29 13:21:44 -0400

    updated line 11 in testdb.php

--- public_html/testdb.php
+++ public_html/testdb.php
@@ -1,19 +1,21 @@
 <?php
 require("config.php");
-$connection_string = "mysql:host=$dbhost;dbname=dbdatabase;charset=utf8mb4";
-try {
-    $db = new PDO($connection_string, $dbhost, $dbuser, $dbpass);
+
+$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+try{
+    $db = new PDO($connection_string, $dbuser, $dbpass);
     $stmt = $db->prepare("CREATE TABLE `Users` (
-                 `id` int auto_increment not null,
-                 `email` varchar(100) not null unique,
-                 `created` timestamp default current_timestamp,
-                PRIMARY KEY (`id`)
-                 ) CHARACTER SET utf8 COLLATE utf8_general_ci");
+				`id` int auto_increment not null,
+				`email` varchar(100) not null unique,
+				`created` timestamp default current_timestamp,
+				`modified` timestamp default current_timestamp on update current_timestamp,
+				PRIMARY KEY (`id`)
+				) CHARACTER SET utf8 COLLATE utf8_general_ci");
     $r = $stmt->execute();
     echo var_export($stmt->errorInfo(), true);
     echo var_export($r, true);
 }
-catch   (Exception $e){
+catch (Exception $e){
     echo $e->getMessage();
 }
 ?>
