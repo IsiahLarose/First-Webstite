aa014019ce6d2c7cc18ddfc9ce0199cf8c0d167a
isiahlarose <iel3@njit.edu>
2020-06-08 15:41:09 -0400

    updated CSS styling sheet test; Will update own version later

--- public_html/RegLoginCSS/header.php
+++ public_html/RegLoginCSS/header.php
@@ -8,3 +8,19 @@
     require("config.php");
     session_start();
 ?>
+<nav>
+    <ul>
+        <li>
+            <a href="home.php">Home</a>
+        </li>
+        <li>
+            <a href="login.php">login</a>
+        </li>
+        <li>
+            <a href="register.php">register</a>
+        </li>
+        <li>
+            <a href="logout.php">logout</a>
+        </li>
+    </ul>
+</nav>
