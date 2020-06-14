c4a92167d027edf18684f1bdede940429e59440e
isiahlarose <iel3@njit.edu>
2020-06-14 15:51:35 -0400

    edited header

--- public_html/RegLoginCSS/header.php
+++ public_html/RegLoginCSS/header.php
@@ -1,46 +1,30 @@
+
 <head>
     <title>
         IT 202 Simple survey
     </title>
     <link rel="stylesheet" style="text/css" href="style.css">
 </head>
-<style>
-    li {
-        float: left;
-    }
-
-    a {
-        display: block;
-        padding: 8px;
-        background-color: none;
-    }
-    ul {
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
-        overflow: hidden;
-    }
-</style>
 <?php
-    require("config.php");
-    session_start();
+require("config.php");
+session_start();
 ?>
-    <nav>
-        <p>
-            Welcome to my Simple Survey!
-        </p>
-        <ul>
-            <li>
-                <a href="home.php">Home</a>
-            </li>
-            <li>
-                <a href="login.php">login</a>
-            </li>
-            <li>
-                <a href="register.php">register</a>
-            </li>
-            <li>
-                <a href="logout.php">logout</a>
-            </li>
-        </ul>
-    </nav>
+<nav>
+    <p>
+        Welcome to my Simple Survey!
+    </p>
+    <ul>
+        <li>
+            <a href="home.php">Home</a>
+        </li>
+        <li>
+            <a href="login.php">login</a>
+        </li>
+        <li>
+            <a href="register.php">register</a>
+        </li>
+        <li>
+            <a href="logout.php">logout</a>
+        </li>
+</ul>
+</nav>
