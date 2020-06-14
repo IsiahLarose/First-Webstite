b407b021787feeebeaf0b371df85e55e47da2490
isiahlarose <iel3@njit.edu>
2020-06-14 16:18:16 -0400

    edited header

--- public_html/RegLoginCSS/home.php
+++ public_html/RegLoginCSS/home.php
@@ -1,4 +1,6 @@
 <?php
 include("header.php"); ?>
 <h4>Home</h4>
-<?php echo "Welcome, " . $_SESSION["user"]["email"];?>
+<?php echo "Welcome, " . $_SESSION["user"]["email"];
+
+"You are sucessfully logged in"?>
