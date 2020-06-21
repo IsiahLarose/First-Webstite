e4642e62eb1e478739526e208436e9c7da66e9b4
isiahlarose <iel3@njit.edu>
2020-06-21 16:18:01 -0400

    edited form

--- public_html/SampleCreateEdit/CreateSurvey.php
+++ public_html/SampleCreateEdit/CreateSurvey.php
@@ -12,14 +12,14 @@
 if(isset($_POST["created"])){
     $Question = $_POST["Question"];
     $Answer = $_POST["Answer"];
-    if(!empty($name) && !empty($Answer)){
+    if(!empty($Question) && !empty($Answer)){
         require("config.php");
         $connection_string = "mysql:host=$dbhost;dbQuestion=$dbdatabase;charset=utf8mb4";
         try{
             $db = new PDO($connection_string, $dbuser, $dbpass);
             $stmt = $db->prepare("INSERT INTO Things (question, Answer) VALUES (:question, :Answer)");
             $result = $stmt->execute(array(
-                ":name" => $name,
+                ":Question" => $Question,
                 ":Answer" => $Answer
             ));
             $e = $stmt->errorInfo();
