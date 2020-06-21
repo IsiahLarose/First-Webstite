6341b68007f4efda54a6b4073861a978db2b09fe
isiahlarose <iel3@njit.edu>
2020-06-21 16:09:08 -0400

    edited form

--- public_html/SampleCreateEdit/CreateSurvey.php
+++ public_html/SampleCreateEdit/CreateSurvey.php
@@ -5,21 +5,21 @@
     <label for="A">Answer
         <input type="text" id="Answer" name="Answer" />
     </label>
-    <input type="submit" Question="created" value="Create Question"/>
+    <input type="submit" name="created" value="Create Question"/>
 </form>
 
 <?php
 if(isset($_POST["created"])){
-    $Question = $_POST["Question"];
+    $name = $_POST["name"];
     $Answer = $_POST["Answer"];
-    if(!empty($Question) && !empty($Answer)){
+    if(!empty($name) && !empty($Answer)){
         require("config.php");
         $connection_string = "mysql:host=$dbhost;dbQuestion=$dbdatabase;charset=utf8mb4";
         try{
             $db = new PDO($connection_string, $dbuser, $dbpass);
-            $stmt = $db->prepare("INSERT INTO Things (Question, Answer) VALUES (:Question, :Answer)");
+            $stmt = $db->prepare("INSERT INTO Things (name, Answer) VALUES (:name, :Answer)");
             $result = $stmt->execute(array(
-                ":Question" => $Question,
+                ":name" => $name,
                 ":Answer" => $Answer
             ));
             $e = $stmt->errorInfo();
@@ -29,7 +29,7 @@ if(isset($_POST["created"])){
             else{
                 echo var_export($result, true);
                 if ($result){
-                    echo "Successfully inserted new thing: " . $Question;
+                    echo "Successfully inserted new thing: " . $name;
                 }
                 else{
                     echo "Error inserting record";
