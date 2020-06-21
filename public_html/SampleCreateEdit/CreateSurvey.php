7cbf9a01aa4da10e5d4143d791f3b5e7dd1a41eb
isiahlarose <iel3@njit.edu>
2020-06-21 16:46:31 -0400

    Removed answer form and made it soley question

--- public_html/SampleCreateEdit/CreateSurvey.php
+++ public_html/SampleCreateEdit/CreateSurvey.php
@@ -1,20 +1,20 @@
 <form method="POST">
-    <label for="Question">Question
-        <input type="text" id="Question" name="Question" />
-    <input type="submit" name="created" value="Create Question"/>
+    <label for="question_text">question_text
+        <input type="text" id="question_text" name="question_text" />
+    <input type="submit" name="created" value="Create question_text"/>
 </form>
 
 <?php
 if(isset($_POST["created"])){
-    $Question = $_POST["Question"];
-    if(!empty($Question)){
+    $question_text = $_POST["question_text"];
+    if(!empty($question_text)){
         require("config.php");
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
         try{
             $db = new PDO($connection_string, $dbuser, $dbpass);
-            $stmt = $db->prepare("INSERT INTO Survey (Question) VALUES (:Question,)");
+            $stmt = $db->prepare("INSERT INTO question_texts (question_text) VALUES (:question_text,)");
             $result = $stmt->execute(array(
-                ":Question" => $Question,
+                ":question_text" => $question_text,
             ));
             $e = $stmt->errorInfo();
             if($e[0] != "00000"){
@@ -23,7 +23,7 @@ if(isset($_POST["created"])){
             else{
                 echo var_export($result, true);
                 if ($result){
-                    echo "Successfully inserted new thing: " . $Question;
+                    echo "Successfully inserted new thing: " . $question_text;
                 }
                 else{
                     echo "Error inserting record";
@@ -35,7 +35,7 @@ if(isset($_POST["created"])){
         }
     }
     else{
-        echo "Question must not be empty.";
+        echo "question_text must not be empty.";
     }
 }
 ?>
