002395ccd4a845648de8edf0451d7378411c851e
isiahlarose <iel3@njit.edu>
2020-06-28 19:31:35 -0400

    syntax error

--- public_html/ajaxqueries/search.php
+++ public_html/ajaxqueries/search.php
@@ -16,7 +16,7 @@ if(isset($_POST["search"])){
         </select>
     </form>
 <?php
-if(($Sort) {
+if(($Sort)) {
         require("common.inc.php");
         $query = file_get_contents(__DIR__ . "/queries/SearchTableASC.sql");
         if (isset($query) && !empty($query)) {
