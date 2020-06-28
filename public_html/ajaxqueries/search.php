491aef39998cf73d7f5ce8d6b319768adff58fec
isiahlarose <iel3@njit.edu>
2020-06-28 13:13:44 -0400

    syntax error

--- public_html/ajaxqueries/search.php
+++ public_html/ajaxqueries/search.php
@@ -22,7 +22,7 @@ if(isset($search)) {
         try {
             $stmt = getDB()->prepare($query);
             //Note: With a LIKE query, we must pass the % during the mapping
-            $stmt->execute([":thing"=>$search]);
+            $stmt->execute([":question"=>$search]);
             //Note the fetchAll(), we need to use it over fetch() if we expect >1 record
             $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
         } catch (Exception $e) {
@@ -41,12 +41,11 @@ if(isset($search)) {
         we're also using our helper function to safely return a value based on our key/column name.-->
         <?php foreach($results as $row):?>
             <li>
-                <?php echo get($row, "name")?>
-                <?php echo get($row, "quantity");?>
-                <a href="delete.php?thingId=<?php echo get($row, "id");?>">Delete</a>
+                <?php echo get($row, "question")?>
+                <a href="delete.php?QuestionId=<?php echo get($row, "id");?>">Delete</a>
             </li>
         <?php endforeach;?>
     </ul>
 <?php else:?>
     <p>This shows when we don't have results</p>
-<?php endif;?
+<?php endif;?>
