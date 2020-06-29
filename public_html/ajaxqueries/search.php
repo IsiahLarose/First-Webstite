34b278c870ecba3de9b3c5dca5b8118e285b875f
isiahlarose <iel3@njit.edu>
2020-06-28 22:53:26 -0400

    missing bracket

--- public_html/ajaxqueries/search.php
+++ public_html/ajaxqueries/search.php
@@ -16,30 +16,27 @@ if(isset($_POST["search"], $_POST["SortBy"])){
         </select>
     </form>
 <?php
-
-if ($Sort["SortBy"] == "Ascending") {
-    $query = file_get_contents(__DIR__ . "/queries/ASC.sql");
-}
-elseif ($Sort["SortBy"] == "Descending") {
-    $query = file_get_contents(__DIR__ . "/queries/DESC.sql");
-}
-    if (isset($query) && !empty($query)) {
+if (isset($query) && !empty($query)) {
+    if($Sort["SortBy"]=="Ascending"){
+        $query = file_get_contents(__DIR__ . "/queries/ASC.sql");
+    }
+    elseif($Sort["SortBy"]=="Descending"){
+        $query = file_get_contents(__DIR__ . "/queries/DESC.sql");
+    }
+if(isset($search)) {
+    require("common.inc.php");
+    $query = file_get_contents(__DIR__ . "/queries/SearchTable.sql");
         try {
             $stmt = getDB()->prepare($query);
             //Note: With a LIKE query, we must pass the % during the mapping
-            $stmt->execute([":question" => $search]);
+            $stmt->execute([":question"=>$search]);
             //Note the fetchAll(), we need to use it over fetch() if we expect >1 record
             $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
         } catch (Exception $e) {
             echo $e->getMessage();
         }
 
-        if ($Sort["SortBy"] == "Ascending") {
-            $query = file_get_contents(__DIR__ . "/queries/ASC.sql");
-        } elseif ($Sort["SortBy"] == "Descending") {
-            $query = file_get_contents(__DIR__ . "/queries/DESC.sql");
-        }
-
+    }
 }
 
 ?>
