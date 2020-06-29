2b99ede6ae94a1dc35bbaaf6b9125d528e607a2c
isiahlarose <iel3@njit.edu>
2020-06-28 22:19:56 -0400

    missing bracket

--- public_html/ajaxqueries/search.php
+++ public_html/ajaxqueries/search.php
@@ -19,30 +19,20 @@ if(isset($_POST["search"], $_POST["SortBy"])){
 
 if(isset($search)) {
     require("common.inc.php");
-    if($Sort["SortBy"]=="Ascending"){
-        $query = file_get_contents(__DIR__ . "/queries/ASC.sql");
-        try {
-            $stmt = getDB()->prepare($query);
-            //Note: With a LIKE query, we must pass the % during the mapping
-            $stmt->execute([":question" => $search]);
-            //Note the fetchAll(), we need to use it over fetch() if we expect >1 record
-            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        } catch (Exception $e) {
-            echo $e->getMessage();
-        }
-    }
-    elseif($Sort["SortBy"]=="Descending") {
-        $query = file_get_contents(__DIR__ . "/queries/DESC.sql");
-        try {
-            $stmt = getDB()->prepare($query);
-            //Note: With a LIKE query, we must pass the % during the mapping
-            $stmt->execute([":question" => $search]);
-            //Note the fetchAll(), we need to use it over fetch() if we expect >1 record
-            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        } catch (Exception $e) {
-            echo $e->getMessage();
-        }
-       if (isset($query) && !empty($query)) {
+    if (isset($query) && !empty($query)) {
+        if ($Sort["SortBy"] == "Ascending") {
+            $query = file_get_contents(__DIR__ . "/queries/ASC.sql");
+            try {
+                $stmt = getDB()->prepare($query);
+                //Note: With a LIKE query, we must pass the % during the mapping
+                $stmt->execute([":question" => $search]);
+                //Note the fetchAll(), we need to use it over fetch() if we expect >1 record
+                $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            } catch (Exception $e) {
+                echo $e->getMessage();
+            }
+        } elseif ($Sort["SortBy"] == "Descending") {
+            $query = file_get_contents(__DIR__ . "/queries/DESC.sql");
             try {
                 $stmt = getDB()->prepare($query);
                 //Note: With a LIKE query, we must pass the % during the mapping
@@ -55,7 +45,6 @@ if(isset($search)) {
         }
     }
 }
-
 ?>
     <!--This part will introduce us to PHP templating,
     note the structure and the ":" -->
