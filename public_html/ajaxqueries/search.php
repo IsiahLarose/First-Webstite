d218f0622a1f882ecfc8001d08d26726774b9acb
isiahlarose <iel3@njit.edu>
2020-06-28 21:35:41 -0400

    changed if loop for form value check

--- public_html/ajaxqueries/search.php
+++ public_html/ajaxqueries/search.php
@@ -19,13 +19,13 @@ if(isset($_POST["search"], $_POST["SortBy"])){
 if(isset($search)) {
     require("common.inc.php");
     $query = file_get_contents(__DIR__ . "/queries/SearchTable.sql");
-    if($Sort["SortBy"]=="Ascending"){
-        $query = file_get_contents(__DIR__ . "/queries/ASC.sql");
-    }
-    elseif($Sort["SortBy"]=="Descending"){
-        $query = file_get_contents(__DIR__ . "/queries/DESC.sql");
-    }
     if (isset($query) && !empty($query)) {
+        if($Sort["SortBy"]=="Ascending"){
+            $query = file_get_contents(__DIR__ . "/queries/ASC.sql");
+        }
+        elseif($Sort["SortBy"]=="Descending"){
+            $query = file_get_contents(__DIR__ . "/queries/DESC.sql");
+        }
         try {
             $stmt = getDB()->prepare($query);
             //Note: With a LIKE query, we must pass the % during the mapping
@@ -37,6 +37,7 @@ if(isset($search)) {
         }
     }
 }
+
 ?>
     <!--This part will introduce us to PHP templating,
     note the structure and the ":" -->
