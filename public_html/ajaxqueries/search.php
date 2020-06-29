1a84ce150eca58667bb0217a2646f0e4979c293f
isiahlarose <iel3@njit.edu>
2020-06-28 22:16:22 -0400

    added missing line into loop

--- public_html/ajaxqueries/search.php
+++ public_html/ajaxqueries/search.php
@@ -19,18 +19,6 @@ if(isset($_POST["search"], $_POST["SortBy"])){
 
 if(isset($search)) {
     require("common.inc.php");
-    $query = file_get_contents(__DIR__ . "/queries/SearchTable.sql");
-    if (isset($query) && !empty($query)) {
-        try {
-            $stmt = getDB()->prepare($query);
-            //Note: With a LIKE query, we must pass the % during the mapping
-            $stmt->execute([":question" => $search]);
-            //Note the fetchAll(), we need to use it over fetch() if we expect >1 record
-            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        } catch (Exception $e) {
-            echo $e->getMessage();
-        }
-    }
     if($Sort["SortBy"]=="Ascending"){
         $query = file_get_contents(__DIR__ . "/queries/ASC.sql");
         try {
@@ -43,7 +31,7 @@ if(isset($search)) {
             echo $e->getMessage();
         }
     }
-    elseif($Sort["SortBy"]=="Descending"){
+    elseif($Sort["SortBy"]=="Descending") {
         $query = file_get_contents(__DIR__ . "/queries/DESC.sql");
         try {
             $stmt = getDB()->prepare($query);
@@ -54,8 +42,18 @@ if(isset($search)) {
         } catch (Exception $e) {
             echo $e->getMessage();
         }
+       /* if (isset($query) && !empty($query)) {
+            try {
+                $stmt = getDB()->prepare($query);
+                //Note: With a LIKE query, we must pass the % during the mapping
+                $stmt->execute([":question" => $search]);
+                //Note the fetchAll(), we need to use it over fetch() if we expect >1 record
+                $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            } catch (Exception $e) {
+                echo $e->getMessage();
+            }
+        }*/
     }
-
 }
 
 ?>
