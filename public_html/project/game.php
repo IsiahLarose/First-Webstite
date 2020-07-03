7aece2ad5bfc8a7f358fc26acabb104fe1e0da3b
isiahlarose <iel3@njit.edu>
2020-07-03 14:47:02 -0400

    modified lives

--- public_html/project/game.php
+++ public_html/project/game.php
@@ -192,11 +192,11 @@
         });
         // Collide the ship with enemies
         enemies.forEach(function(enemy, i) {
-           $life = 2;
+           let life = 2;
             if (isColliding(enemy, ship)) {
-                --$life;
+                life=life-1;
             }
-            if($life == 0){
+            if(life == 0){
                 gameOver = true;
             }
         });
