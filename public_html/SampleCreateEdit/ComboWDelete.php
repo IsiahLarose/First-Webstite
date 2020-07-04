6980c30265b30e42fb46be7f6666d90fe5b3bb2f
isiahlarose <iel3@njit.edu>
2020-07-04 14:47:36 -0400

    changed is_numeric to is_string

--- public_html/SampleCreateEdit/ComboWDelete.php
+++ public_html/SampleCreateEdit/ComboWDelete.php
@@ -28,9 +28,8 @@ else{
         <label for="Question">Question Name
             <input type="text" id="Question" name="Question" value="<?php echo get($result, "Question");?>" />
         </label>
-        <label for = "Answer"
+        <label for ="Answer">Answer
                <input type = "text" id = "Answer" name = "Answer" value = "<?php echo get($result, "Question");?>"
-
         </label>
         <?php if($QuestionId > 0):?>
             <input type="submit" name="updated" value="Update Question"/>
