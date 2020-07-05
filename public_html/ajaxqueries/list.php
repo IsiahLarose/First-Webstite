f1f7d1ab824970f9da6d84d3a61370070d7996ea
isiahlarose <iel3@njit.edu>
2020-07-05 14:45:10 -0400

    list.php edit

--- public_html/ajaxqueries/list.php
+++ public_html/ajaxqueries/list.php
@@ -26,23 +26,23 @@ if(isset($query) && !empty($query) && isset($query2) && !empty($query2)){
     <!--This part will introduce us to PHP templating,
     note the structure and the ":" -->
     <!-- note how we must close each check we're doing as well-->
-<?php if(isset($results)):?>
-    <p>This shows when we have results</p>
-    <ul>
-        <!-- Here we'll loop over all our results and reuse a specific template for each iteration,
-        we're also using our helper function to safely return a value based on our key/column name.-->
-        <?php echo" Questions"?>
-        <?php foreach($results as $row):?>
-            <li>
-                <?php echo get($row, "question,")?>
-                <a href="delete.php?QuestionId=<?php echo get($row, "id");?>">Delete</a>
-            </li>
+<<?php if(isset($results)):?>
+<p>This shows when we have results</p>
+<ul>
+    <!-- Here we'll loop over all our results and reuse a specific template for each iteration,
+    we're also using our helper function to safely return a value based on our key/column name.-->
+    <?php foreach($results as $row):?>
+        <li>
+            <?php echo" Questions"?>
 
-            <li>
-                <?php echo get($row, "answer")?>
-                <a href="delete.php?QuestionId=<?php echo get($row, "id");?>">Delete</a>
-            </li>
-        <?php endforeach;?>
+            <?php echo get($row, "question")?>
+            <?php echo""?>
+            <?php echo" Matching Answers"?>
+            <?php echo get($row, "answer")?>
+            <a href="delete.php?QuestionId=<?php echo get($row, "id");?>">Delete</a>
+        </li>
+    <?php endforeach;?>
+</ul>
 <?php else:?>
     <p>This shows when we don't have results</p>
 <?php endif;?>
