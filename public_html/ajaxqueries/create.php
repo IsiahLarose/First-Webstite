fd1b607018b38a8cc7b26aff232cdac42c106afc
isiahlarose <iel3@njit.edu>
2020-07-05 13:37:07 -0400

    updated combo form

--- public_html/ajaxqueries/create.php
+++ public_html/ajaxqueries/create.php
@@ -17,7 +17,6 @@ if(isset($_POST["created"])){
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
         try{
             $db = new PDO($connection_string, $dbuser, $dbpass);
-            $result = $stmt->execute();
             $stmt = $db->prepare("INSERT INTO Questions (question) VALUES (:question)");
             $result = $stmt->execute(array(
                 ":question" => $question,
