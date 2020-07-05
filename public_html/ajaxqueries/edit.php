2306713f9ba8ce93c4bdbfeac4c67895ca559e63
isiahlarose <iel3@njit.edu>
2020-07-05 16:44:06 -0400

    edit.php tweak

--- public_html/ajaxqueries/edit.php
+++ public_html/ajaxqueries/edit.php
@@ -1,86 +1,41 @@
 <?php
-$questionId = -1;
-$answerId = -1;
-if(isset($_GET["questionId"]) && !empty($_GET["questionId"])){
-    $questionId = $_GET["questionId"];
-
-}
-if(isset($_GET["answerId"]) && !empty($_GET["answerId"])) {
-    $questionId = $_GET["answerId"];
-}
-$result = array();
-require("common.inc.php");
-?>
-<?php
-if(isset($_POST["updated"])){
-    $question = "";
-    $answer = "";
-    if(isset($_POST["question"]) && !empty($_POST["question"])){
-        $question = $_POST["question"];
-    }
-    if(isset($_POST["answer"]) && !empty($_POST["answer"])) {
-        if (is_string($_POST["answer"])) {
-            $answer = (string)$_POST["answer"];
-        }
-    }
+if(isset($_POST["created"])){
+    $question = $_POST["Question"];
+    $answer = $_POST["Answer"];
     if(!empty($question) && !empty($answer)){
+        require("config.php");
+        $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
         try{
-            $query = NULL;
-            echo "[answer" . $answer . "]";
-            if(isset($query) && !empty($query)) {
-                $stmt = getDB()->prepare("UPDATE Questions set question = :question where id=:id");
-                $result = $stmt->execute();
-                $stmt = getDB()->prepare("UPDATE Answers set answer = :answer where id=:id");
-                $result = $stmt->execute(
-                );
-                $e = $stmt->errorInfo();
-                if ($e[0] != "00000") {
-                    echo var_export($e, true);
-                } else {
-                    if ($result) {
-                        echo "Successfully updated question: " . $question;
-                    } else {
-                        echo "Error updating record";
-                    }
-                }
+            $db = new PDO($connection_string, $dbuser, $dbpass);
+            $stmt = $db->prepare("UPDATE Questions SET (question) VALUES (:question)");
+            $result = $stmt->execute(array(
+                ":question" => $question,
+            ));
+            $stmt = $db->prepare("UPDATE Answers SET (answer) VALUES (:answer)");
+            $result = $stmt->execute(array(
+                ":answer" => $answer,
+            ));
+            $e = $stmt->errorInfo();
+            if($e[0] != "00000"){
+                echo var_export($e, true);
             }
             else{
-                echo "Failed to find Update.sql file";
+                echo var_export($result, true);
+                if ($result){
+                    echo "Successfully inserted new Question & Answer " . $question;
+                }
+                else{
+                    echo "Error inserting record";
+                }
             }
         }
         catch (Exception $e){
             echo $e->getMessage();
         }
     }
-
     else{
-        echo "Name and answer must not be empty.";
+        echo "Question must not be empty.";
     }
-}
-?>
-
-<?php
-//moved the content down here so it pulls the update from the table without having to refresh the page or redirect
-//now my success message appears above the form so I'd have to further restructure my code to get the desired output/layout
-if($questionId > -1){
-    $query = file_get_contents(__DIR__ . "/queries/SelectOne.sql");
-    if(isset($query) && !empty($query)) {
-        //Note: SQL File contains a "LIMIT 1" although it's not necessary since ID should be unique (i.e., one record)
-        try {
-            $stmt = getDB()->prepare($query);
-            $stmt->execute([":id" => $questionId]);
-            $result = $stmt->fetch(PDO::FETCH_ASSOC);
-        }
-        catch (Exception $e){
-            echo $e->getMessage();
-        }
-    }
-    else{
-        echo "Failed to find SelectOne.sql file";
-    }
-}
-else{
-    echo "No questionId provided in url, don't forget this or sample won't work.";
 }
 ?>
 <script src="js/script.js"></script>
