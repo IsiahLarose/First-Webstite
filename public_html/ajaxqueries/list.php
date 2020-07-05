b5910bc5a48920041735936d61aae0f3b78444c6
isiahlarose <iel3@njit.edu>
2020-07-05 14:56:03 -0400

    list.php edit

--- public_html/ajaxqueries/list.php
+++ public_html/ajaxqueries/list.php
@@ -26,7 +26,7 @@ if(isset($query) && !empty($query) && isset($query2) && !empty($query2)){
     <!--This part will introduce us to PHP templating,
     note the structure and the ":" -->
     <!-- note how we must close each check we're doing as well-->
-<<?php if(isset($results)):?>
+<?php if(isset($results)):?>
 <p>This shows when we have results</p>
 <ul>
     <!-- Here we'll loop over all our results and reuse a specific template for each iteration,
