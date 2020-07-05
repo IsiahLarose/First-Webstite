b76fec0e999153eb279e13497c69c325ff23fec5
isiahlarose <iel3@njit.edu>
2020-07-05 18:35:35 -0400

    CSS styling edit

--- public_html/ajaxqueries/edit.php
+++ public_html/ajaxqueries/edit.php
@@ -1,6 +1,5 @@
 <head>
     <title>Simple Survey</title>
-    <link rel="stylesheet" type="text/css" href="style2.css">
     <p>
         Edit your Question below
     </p>
@@ -120,7 +119,6 @@ else{
 <script src="js/script.js"></script>
 <!-- note although <script> tag "can" be self terminating some browsers require the
 full closing tag-->
-<head>
 <form method="POST"onsubmit="return validate(this);">
     <label for="question">question Name
         <!-- since the last assignment we added a required attribute to the form elements-->
@@ -131,10 +129,3 @@ full closing tag-->
     </label>
     <input type="submit" name="updated" value="Update question & answer"/>
 </form>
-
-    <title>Simple Survey</title>
-    <link rel="stylesheet" type="text/css" href="style2.css">
-    <p>
-        Edit your Question below
-    </p>
-</head>
