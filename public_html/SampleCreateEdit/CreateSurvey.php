954ca664391d31995e8855524ced724b554467cb
isiahlarose <iel3@njit.edu>
2020-07-04 21:53:24 -0400

    3

--- public_html/SampleCreateEdit/CreateSurvey.php
+++ public_html/SampleCreateEdit/CreateSurvey.php
@@ -12,14 +12,43 @@
 if(isset($_POST["created"])){
     $question = $_POST["Question"];
     $answer = $_POST["Answer"];
-    if(!empty($question) && !empty($answer)){
+    if(!empty($question)){
         require("config.php");
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
         try{
             $db = new PDO($connection_string, $dbuser, $dbpass);
-            $stmt = $db->prepare("INSERT INTO Question (question) VALUES (:question) and  INSERT  into Answers (answer) VALUES (:answer)");
+            $stmt = $db->prepare("INSERT INTO Questions (question) VALUES (:question) ");
             $result = $stmt->execute(array(
-                ":question" => $question,
+                ":question" => $question
+            ));
+            $e = $stmt->errorInfo();
+            if($e[0] != "00000"){
+                echo var_export($e, true);
+            }
+            else{
+                echo var_export($result, true);
+                if ($result){
+                    echo "Successfully inserted new Question & Answer " . $question;
+                }
+                else{
+                    echo "Error inserting record";
+                }
+            }
+        }
+        catch (Exception $e){
+            echo $e->getMessage();
+        }
+    }
+    else{
+        echo "Question must not be empty.";
+    }
+    if(!empty($answer)){
+        require("config.php");
+        $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+        try{
+            $db = new PDO($connection_string, $dbuser, $dbpass);
+            $stmt = $db->prepare("INSERT INTO Answers (answer) VALUES (:answer) ");
+            $result = $stmt->execute(array(
                 ":answer" => $answer
             ));
             $e = $stmt->errorInfo();
