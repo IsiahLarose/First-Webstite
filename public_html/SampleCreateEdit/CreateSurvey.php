f7438c30dd927b2a209b7510d7885b2ca4ae367b
isiahlarose <iel3@njit.edu>
2020-07-04 23:32:10 -0400

    tweaked CreateSurvey.php

--- public_html/SampleCreateEdit/CreateSurvey.php
+++ public_html/SampleCreateEdit/CreateSurvey.php
@@ -2,13 +2,13 @@
 <!-- note although <script> tag "can" be self terminating some browsers require the
 full closing tag-->
 <form method="POST" onsubmit="return validate(this);">
-    <label for="thing">Question Name
-        <input type="text" id="thing" name="Question" required />
+    <label for="Question">Question Name
+        <input type="text" id="Question" name="Question" required />
     </label>
-    <label for="q">Answer
-        <input type="text" id="Answer" name="Answer" required min="0" />
-    </label>
-    <input type="submit" name="created" value="Create Question"/>
+        <label for="Answer">Answer
+            <input type="text" id="q-Answer" name="Answer" required />
+        </label>
+        <input type="submit" name="created" value="Create Question"/>
 </form>
 <?php
 if(isset($_POST["created"])) {
@@ -17,42 +17,42 @@ if(isset($_POST["created"])) {
     if(isset($_POST["Question"]) && !empty($_POST["Question"])){
         $Question = $_POST["Question"];
     }
-    if(isset($_POST["$Answer"]) && !empty($_POST["$Answer"])){
-        if(is_string($_POST["$Answer"])){
-            $$Answer = (string)$_POST["$Answer"];
-        }
-    }
-    //If Question or $Answer is invalid, don't do the DB part
-    if(empty($Question) || $Answer ){
-        echo "Name or Answer can't be empty";
-        die();//terminates the rest of the script
-    }
-    try {
-        require("common.inc.php");
-        $query = file_get_contents(__DIR__ . "/queries/InsertInto.sql");
-        if(isset($query) && !empty($query)) {
-            $stmt = getDB()->prepare($query);
-            $result = $stmt->execute(array(
-                ":Question" => $Question,
-                ":$Answer" => $Answer
+            if(isset($_POST["Answer"]) && !empty($_POST["Answer"])){
+                if(is_numeric($_POST["Answer"])){
+                    $Answer = (int)$_POST["Answer"];
+                }
+            }
+            //If field is invalid, don't do the DB part
+                if(empty($Question) || $Answer < 0 ){
+                    echo "Question must not be empty and Answer must be greater than or equal to 0";
+                    die();//terminates the rest of the script
+                }
+                try {
+                    require("common.inc.php");
+                    $query = file_get_contents(__DIR__ . "/queries/InsertInto.sql");
+                    if(isset($query) && !empty($query)) {
+                        $stmt = getDB()->prepare($query);
+                        $result = $stmt->execute(array(
+                            ":Question" => $Question,
+                ":Answer" => $Answer
             ));
             $e = $stmt->errorInfo();
             if ($e[0] != "00000") {
                 echo var_export($e, true);
             } else {
                 if ($result) {
-                    echo "Successfully inserted new thing: " . $Question;
+                    echo "Successfully inserted new Question: " . $Question;
                 } else {
                     echo "Error inserting record";
                 }
             }
         }
-        else{
-            echo "Failed to find INSERT_TABLE_THINGS.sql file";
-        }
-    }
-    catch (Exception $e){
-        echo $e->getMessage();
-    }
-}
-?>
+                    else{
+                        echo "Failed to find InsertInto.sql file";
+                    }
+                }
+                catch (Exception $e){
+                    echo $e->getMessage();
+                }
+            }
+            ?>
