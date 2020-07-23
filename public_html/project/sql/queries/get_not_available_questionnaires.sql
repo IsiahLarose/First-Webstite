a9e8b2ecb072406c55919989d43a8f49755eb228
isiahlarose <iel3@njit.edu>
2020-07-23 17:49:25 -0400

    edited query to select surveys not taken

--- public_html/project/sql/queries/get_not_available_questionnaires.sql
+++ public_html/project/sql/queries/get_not_available_questionnaires.sql
@@ -4,4 +4,4 @@ FROM Responses where user_id = :uid and questionnaire_id = q.id and date(created
 and
 q.use_max = 0)
 or
-(q.use_max = 1 and q.max_attempts < (select COUNT(1) from Responses where user_id = :uid and questionnaire_id = q.id))
+(q.use_max = 1 and q.max_attempts < (select COUNT(1) from Responses where user_id = :uid and questionnaire_id = null ))
--- public_html/project/surveys.php
+++ public_html/project/surveys.php
@@ -33,3 +33,41 @@ if(Common::get($response, "status", 400) == 200){
         <?php endif; ?>
     </div>
 </div>
+
+<?php
+$limit = 2;
+$query = "SELECT count(*) FROM kategori";
+
+$s = $db->query($query);
+$total_results = $s->fetchColumn();
+$total_pages = ceil($total_results/$limit);
+
+if (!isset($_GET['page'])) {
+    $page = 1;
+} else{
+    $page = $_GET['page'];
+}
+
+$starting_limit = ($page-1)*$limit;
+$show  = "SELECT * FROM Questions ORDER BY id DESC LIMIT ?,?";
+
+$r = $db->prepare($show);
+$r->execute([$starting_limit, $limit]);
+
+while($res = $r->fetch(PDO::FETCH_ASSOC)):
+    ?>
+    <h4><?php echo $s['id'];?></h4>
+    <p><?php echo $s['name'];?></p>
+    <h4><?php echo $s['description'];?></h4>
+    <p><?php echo $s['use_max'];?></p><h4>
+    <p><?php echo $s['max_attempts'];?></p>
+    <hr>
+<?php
+endwhile;
+
+for ($page=1; $page <= $total_pages ; $page++):?>
+
+    <a href='<?php echo "?page=$page"; ?>' class="links"><?php  echo $page; ?>
+    </a>
+
+<?php endfor; ?>
