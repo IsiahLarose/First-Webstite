f44cedf3d2f18b313c8926d89c4f1e237f942ccc
isiahlarose <iel3@njit.edu>
2020-07-23 12:02:44 -0400

    edited query to select surveys not taken

--- public_html/project/sql/queries/get_not_available_questionnaires.sql
+++ public_html/project/sql/queries/get_not_available_questionnaires.sql
@@ -1,1 +1,4 @@
-SELECT * from Questionnaires Where question_id NOT in TABLE Responses
+SELECT Questionnaires.question_id
+FROM Questionnaires t1
+LEFT JOIN Responses t2 ON t2.question_id = t1.name
+WHERE t2.question_id IS NULL
