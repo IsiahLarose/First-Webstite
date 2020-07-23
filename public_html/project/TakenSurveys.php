71eb463a172f72583b3b24876409695ce015a578
isiahlarose <iel3@njit.edu>
2020-07-23 18:15:22 -0400

    Pagination Test

--- public_html/project/TakenSurveys.php
+++ public_html/project/TakenSurveys.php
@@ -5,7 +5,7 @@ if(Common::is_logged_in()){
     //this will auto redirect if user isn't logged in
 }
 //TODO: Note, internally calling them questionnaires (and for admin), user facing they're called surveys.
-$response = DBH::get_not_available_surveys();
+$response = DBH::get_available_surveys();
 $available = [];
 if(Common::get($response, "status", 400) == 200){
     $available = Common::get($response, "data", []);
@@ -14,16 +14,40 @@ if(Common::get($response, "status", 400) == 200){
 <div class="container-fluid">
     <h4>Surveys</h4>
     <div class="list-group">
-        <?php foreach($available  as $s): ?>
+        <?php foreach($available as $s): ?>
             <div class="list-group-item">
-                <h6><?php echo Common::get($s, "name", ""); ?></h6>
-                <p><?php echo Common::get($s, "description", ""); ?></p>
-                <?php if(Common::get($s, "use_max", false)): ?>
-                    <div>Max Attempts: <?php echo Common::get($s, "max_attempts", 0);?></div>
-                <?php else:?>
-                    <div>Daily Attempts: <?php echo Common::get($s, "attempts_per_day", 0);?></div>
-                <?php endif; ?>
-                <a href="survey.php?s=<?php echo Common::get($s, 'id', -1);?>" class="btn btn-secondary">Participate</a>
+                <?php
+                $limit = 2;
+                $query = "SELECT count(*) FROM Questionnaires";
+
+                $s = $db->query($query);
+                $total_results = $s->fetchColumn();
+                $total_pages = ceil($total_results/$limit);
+                if (!isset($_GET['page'])) {
+                    $page = 1;
+                } else{
+                    $page = $_GET['page'];
+                }
+                $starting_limit = ($page-1)*$limit;
+                $show  = "SELECT * FROM Questionnaire ORDER BY id DESC LIMIT ?,?";
+
+                $r = $db->prepare($show);
+                $r->execute([$starting_limit, $limit]);
+
+                while($available = $s->fetch(PDO::FETCH_ASSOC)):
+                    ?>
+                    <h4><?php echo $re['id'];?></h4>
+                    <p><?php echo $res['nama_kat'];?></p>
+                    <hr>
+                <?php
+                endwhile;
+                for ($page=1; $page <= $total_pages ; $page++):?>
+
+                    <a href='<?php echo "?page=$page"; ?>' class="links"><?php  echo $page; ?>
+                    </a>
+
+                <?php endfor; ?>
+
             </div>
         <?php endforeach; ?>
         <?php if(count($available) == 0):?>
@@ -33,3 +57,5 @@ if(Common::get($response, "status", 400) == 200){
         <?php endif; ?>
     </div>
 </div>
+
+
