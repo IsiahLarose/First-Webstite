aee1b556f158dac8ed49ce1bfddf6d1a4c4a56a7
isiahlarose <iel3@njit.edu>
2020-07-23 11:15:46 -0400

    added query to get surveys that have been taken already

--- public_html/project/sql/queries/get_not_available_questionnaires.sql
+++ public_html/project/sql/queries/get_not_available_questionnaires.sql
@@ -1,3 +1,1 @@
-SELECT * from Questionnaires as q where
-(attempts_per_day  (SELECT COUNT(1)
-FROM Responses where user_id = :uid and questionnaire_id = q.id and date(created) = CURDATE())
+SELECT * from Questionnaires
