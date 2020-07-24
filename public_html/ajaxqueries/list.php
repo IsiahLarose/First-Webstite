157bf4ba3222562d8002a47ceabd0bdb02290a2b
isiahlarose <iel3@njit.edu>
2020-07-24 17:47:23 -0400

    added ability for user to view survey results *work in progress*

---
+++ public_html/ajaxqueries/list.php
@@ -0,0 +1,33 @@
+<?php
+require("common.inc.php");
+if(isset($query) && !empty($query)){
+    require("config.php");
+    $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+    try {
+        $stmt = getDB()->prepare("Select * FROM Questionnaires");
+        $stmt->execute();
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    catch (Exception $e){
+        echo $e->getMessage();
+    }
+}
+?>
+    <!--This part will introduce us to PHP templating,
+    note the structure and the ":" -->
+    <!-- note how we must close each check we're doing as well-->
+<?php if(isset($results)):?>
+    <p>This shows when we have results</p>
+    <ul>
+        <!-- Here we'll loop over all our results and reuse a specific template for each iteration,
+        we're also using our helper function to safely return a value based on our key/column name.-->
+        <?php foreach($results as $row):?>
+            <li>
+                <?php echo get($row, "question_id");?>
+                <?php echo get($row, "user_id");?>
+            </li>
+        <?php endforeach;?>
+    </ul>
+<?php else:?>
+    <p>This shows when we don't have results</p>
+<?php endif;?>
