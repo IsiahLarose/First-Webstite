36915d7dd3557e5f37e13e184f0fcba42873995f
isiahlarose <iel3@njit.edu>
2020-07-26 19:40:28 -0400

    added search ability

--- public_html/project/partials/header.partial.php
+++ public_html/project/partials/header.partial.php
@@ -5,11 +5,7 @@ $logged_in = Common::is_logged_in(false);
 <!-- Bootstrap 4 CSS only -->
 <link rel="stylesheet" type="text/css" href="Styling.css">
 <!-- Include jQuery 3.5.1-->
-<script
-        src="https://code.jquery.com/jquery-3.5.1.min.js"
-        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-        crossorigin="anonymous"></script>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav>
     <ul class="navbar-nav mr-auto">
         <?php if($logged_in):?>
             <li class="nav-item">
