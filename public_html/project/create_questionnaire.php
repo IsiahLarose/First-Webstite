9d5af5a6fd1983a1466ad93c691d71e5c668e078
isiahlarose <iel3@njit.edu>
2020-07-26 22:08:59 -0400

    added search ability

--- public_html/project/create_questionnaire.php
+++ public_html/project/create_questionnaire.php
@@ -3,9 +3,6 @@ include_once(__DIR__."/partials/header.partial.php");
 
 if(Common::is_logged_in()){
     //this will auto redirect if user isn't logged in
-    if(!Common::has_role("Admin")){
-        die(header("Location: home.php"));
-    }
 }
 $last_updated = Common::get($_SESSION, "last_sync", false);
 ?>
