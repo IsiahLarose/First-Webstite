ed38abde01b09f1d14477665499745564ad247fa
isiahlarose <iel3@njit.edu>
2020-07-26 23:23:29 -0400

    added search ability

--- public_html/project/partials/header.partial.php
+++ public_html/project/partials/header.partial.php
@@ -9,7 +9,7 @@ $logged_in = Common::is_logged_in(false);
         src="https://code.jquery.com/jquery-3.5.1.min.js"
         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
         crossorigin="anonymous"></script>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-light bg-success">
     <ul class="navbar-nav mr-auto">
         <?php if($logged_in):?>
             <li class="nav-item">
--- public_html/project/sql/queries/Results.sql
+++ public_html/project/sql/queries/Results.sql
@@ -1,1 +1,8 @@
-Select * FROM Questionnaires
+SELECT
+    question_id, user_id, COUNT(*)
+FROM
+    Responses
+GROUP BY
+    question_id, user_id
+HAVING
+    COUNT(*) > 1
