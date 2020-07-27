ac6d0088d25d13242da10a89c9532568f4176977
isiahlarose <iel3@njit.edu>
2020-07-26 21:15:57 -0400

    added search ability

--- public_html/project/partials/header.partial.php
+++ public_html/project/partials/header.partial.php
@@ -3,13 +3,12 @@ require_once (__DIR__."/../includes/common.inc.php");
 $logged_in = Common::is_logged_in(false);
 ?>
 <!-- Bootstrap 4 CSS only -->
-<link rel="stylesheet" href="Styling.css">
-<!-- Include jQuery 3.5.1-->
-<script
-        src="https://code.jquery.com/jquery-3.5.1.min.js"
-        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-        crossorigin="anonymous"></script>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous">
+</script><!-- Include jQuery 3.5.1-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-light">
     <ul class="navbar-nav mr-auto">
         <?php if($logged_in):?>
             <li class="nav-item">
