bfa7e388a0fb4d37b34a675b4109bb7a9ea6b8d3
isiahlarose <iel3@njit.edu>
2020-07-26 21:31:55 -0400

    styling

---
+++ public_html/project/header.php
@@ -0,0 +1,22 @@
+<head>
+
+    <title>Simple Survey</title>
+
+    <link rel="stylesheet" type="text/css" href="Styling.css">
+
+</head>
+
+<?php
+
+require("config.php");
+
+session_start();
+
+?>
+
+<ul>
+    <li><a href="register.php">Register</a></li>
+
+    <li><a href="login.php">Login</a></li>
+
+</ul>
