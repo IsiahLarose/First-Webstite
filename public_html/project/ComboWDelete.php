1f23b3b6313d2545b0b8bdef5bd4dbf127e9a84c
isiahlarose <iel3@njit.edu>
2020-08-03 11:22:19 -0400

    added edit function to nav bar

--- public_html/project/ComboWDelete.php
+++ public_html/project/ComboWDelete.php
@@ -10,63 +10,63 @@ function get($arr, $key){
     }
     return "";
 }
-if(isset($_GET["QuestionId"])){
-    $QuestionId = $_GET["QuestionId"];
-    $stmt = $db->prepare("SELECT * FROM Questions where id = :id");
-    $stmt->execute([":id"=>$QuestionId]);
+if(isset($_GET["QuestionnaireId"])){
+    $QuestionnaireId = $_GET["QuestionId"];
+    $stmt = $db->prepare("SELECT * FROM Questionnaires where id = :id");
+    $stmt->execute([":id"=>$QuestionnaireId]);
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
     if(!$result){
-        $QuestionId = -1;
+        $QuestionnaireId = -1;
     }
 }
 else{
-    echo "No QuestionId provided in url, don't forget this or sample won't work.";
+    echo "No QuestionnaireId provided in url, don't forget this or sample won't work.";
 }
 ?>
 
     <form method="POST">
-        <label for="Question">Question Name
-            <input type="text" id="Question" name="Question" value="<?php echo get($result, "Question");?>" />
+        <label for="Questionnaire">Questionnaire Name
+            <input type="text" id="Questionnaire" name="Questionnaire" value="<?php echo get($result, "Questionnaire");?>" />
         </label>
-        <label for ="Answer">Answer
-               <input type = "text" id = "Answer" name = "Answer" value = "<?php echo get($result, "Question");?>"
+        <label for ="description">description
+               <input type = "text" id = "description" name = "description" value = "<?php echo get($result, "Questionnaire");?>"
         </label>
-        <?php if($QuestionId > 0):?>
-            <input type="submit" name="updated" value="Update Question"/>
-            <input type="submit" name="delete" value="Delete Question"/>
-        <?php elseif ($QuestionId < 0):?>
-            <input type="submit" name="created" value="Create Question"/>
+        <?php if($QuestionnaireId > 0):?>
+            <input type="submit" name="updated" value="Update Questionnaire"/>
+            <input type="submit" name="delete" value="Delete Questionnaire"/>
+        <?php elseif ($QuestionnaireId < 0):?>
+            <input type="submit" name="created" value="Create Questionnaire"/>
         <?php endif;?>
     </form>
 
 <?php
 if(isset($_POST["updated"]) || isset($_POST["created"]) || isset($_POST["delete"])){
     $delete = isset($_POST["delete"]);
-    $Question = $_POST["Question"];
-    $Answer = $_POST["Answer"];
-    if(!empty($Question) && !empty($Answer)){
+    $Questionnaire = $_POST["Questionnaire"];
+    $description = $_POST["description"];
+    if(!empty($Questionnaire) && !empty($description)){
         try{
-            if($QuestionId > 0) {
+            if($QuestionnaireId > 0) {
                 if($delete){
-                    $stmt = $db->prepare("DELETE from Questions where id=:id");
+                    $stmt = $db->prepare("DELETE from Questionnaires where id=:id");
                     $result = $stmt->execute(array(
-                        ":id" => $QuestionId,
+                        ":id" => $QuestionnaireId,
                     ));
                 }
                 else {
-                    $stmt = $db->prepare("UPDATE Questions set Question = :Question where id=:id");
+                    $stmt = $db->prepare("UPDATE Questionnaires set Questionnaire = :Questionnaire where id=:id");
                     $result = $stmt->execute(array(
-                        ":Question" => $Question,
-                        ":Answer" => $Answer,
-                        ":id" => $QuestionId
+                        ":Questionnaire" => $Questionnaire,
+                        ":description" => $description,
+                        ":id" => $QuestionnaireId
                     ));
                 }
             }
             else{
-                $stmt = $db->prepare("INSERT INTO Questions (Question) VALUES (:Question)");
+                $stmt = $db->prepare("INSERT INTO Questionnaires (Questionnaire) VALUES (:Questionnaire)");
                 $result = $stmt->execute(array(
-                    ":Question" => $Question,
-                    ":Answer" => $Answer
+                    ":Questionnaire" => $Questionnaire,
+                    ":description" => $description
                 ));
             }
             $e = $stmt->errorInfo();
@@ -76,7 +76,7 @@ if(isset($_POST["updated"]) || isset($_POST["created"]) || isset($_POST["delete"
             else{
                 echo var_export($result, true);
                 if ($result){
-                    echo "Successfully interacted with Question: " . $Question;
+                    echo "Successfully interacted with Questionnaire: " . $Questionnaire;
                 }
                 else{
                     echo "Error interacting record";
@@ -88,7 +88,7 @@ if(isset($_POST["updated"]) || isset($_POST["created"]) || isset($_POST["delete"
         }
     }
     else{
-        echo "Question & Answer must not be empty.";
+        echo "Questionnaire & description must not be empty.";
     }
 }
 ?>
