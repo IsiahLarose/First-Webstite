da05743cc57a3f19de72e538c5d42cc18381b35d
isiahlarose <iel3@njit.edu>
2020-08-03 00:40:49 -0400

    Pagination test

--- public_html/project/home.php
+++ public_html/project/home.php
@@ -1,10 +1,10 @@
 <?php
 include_once(__DIR__."/partials/header.partial.php");
-
 if(Common::is_logged_in()){
     //this will auto redirect if user isn't logged in
 }
 ?>
+<link rel="stylesheet" type="text/css" href="style.css">
 <div>
     <p>Welcome, <?php echo Common::get_username();?></p>
 </div>
